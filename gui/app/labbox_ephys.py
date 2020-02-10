15e94cb3b0476582603ef381f5fee31965d7b778
Jeremy Magland <jmagland@flatironinstitute.org>
2020-02-10 15:38:41 -0500

    remove write to test

--- gui/app/labbox_ephys.py
+++ gui/app/labbox_ephys.py
@@ -6,7 +6,5 @@ lew.init_electron()
 X = lew.MainWidget(
     ephys_data_dir=os.getenv('EPHYS_DATA_DIR', '')
 )
-print('writing --- to /kachery-storage')
-with open('/kachery-storage/testA.txt', 'w') as f:
-    f.write('this is a test from inside the container')
+
 X.show()
